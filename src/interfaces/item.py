f7209d711550a88f7a58879f0ea105eb1335af26
kimal <kimalmagix@gmail.com>
2025-12-21 03:28:21 +0800

    uploaded

--- src/interfaces/item.py
+++ src/interfaces/item.py
@@ -1,8 +1,16 @@
 class Item:
+    """
+    Class representing a student.
+    """
     def __init__(self, student_id: str, name: str, group: str):
         self.student_id = student_id
         self.name = name
         self.group = group
 
     def __repr__(self):
-        return f"Item({self.student_id}, {self.name}, {self.group})"
+        return f"Item(ID='{self.student_id}', Name='{self.name}', Group='{self.group}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Item):
+            return NotImplemented
+        return self.student_id == other.student_id
--- src/storages/dict_storage.py
+++ src/storages/dict_storage.py
@@ -1,24 +1,6 @@
 # src/storages/dict_storage.py
 from typing import Any, Dict, List, Optional
 
-# --- Item Class (Simplified based on the new plan) ---
-class Item:
-    """
-    Class representing a student.
-    """
-    def __init__(self, student_id: str, name: str, group: str):
-        self.student_id = student_id
-        self.name = name
-        self.group = group
-
-    def __repr__(self):
-        return f"Item(ID='{self.student_id}', Name='{self.name}', Group='{self.group}')"
-
-    def __eq__(self, other):
-        if not isinstance(other, Item):
-            return NotImplemented
-        return self.student_id == other.student_id
-
 
 class DictStorage:
     """
